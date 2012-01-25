ace2fd65ee6f5ca93b9d0d8dcb6690cb10cba553
Daniel Gerlanc <dgerlanc@gmail.com>
2012-01-25 10:57:25 -0500

    Assigned default values of NULL to 'data.col' and 'grp.col'.
    Fixed incorrect check for missingness of contrasts argument. 'contrasts' is NULL by default.

--- R/bootES.R
+++ R/bootES.R
@@ -1,7 +1,7 @@
 ## Daniel Gerlanc and Kris Kirby (2010)
 ## High-level function for bootstrap analyses using the 'boot' package
 
-bootES <- function(dat, R=1000, data.col, grp.col,
+bootES <- function(dat, R=1000, data.col=NULL, grp.col=NULL,
                    effect.type=c("unstandardized", "cohens.d", "hedges.g",
                      "cohens.d.sigma", "r", "slope"),
                    contrasts=NULL,                   
@@ -69,7 +69,7 @@ bootES <- function(dat, R=1000, data.col, grp.col,
     stop("R must be an integer of length 1 and greater than 0")
   
   ## Check and extract 'data.col'.
-  if (!missing(data.col)) {
+  if (!is.null(data.col)) {
     if (!is.character(data.col))
       stop("'data.col' must be a character vector.")
   
@@ -81,7 +81,7 @@ bootES <- function(dat, R=1000, data.col, grp.col,
   
   ## Check and extract 'grp.col'.
   grps = NULL
-  if (!missing(grp.col)) {
+  if (!is.null(grp.col)) {
     if (!is.character(grp.col)) 
       stop("'grp.col' must be a character vector.")
     
@@ -97,7 +97,7 @@ bootES <- function(dat, R=1000, data.col, grp.col,
   
   ## Check and extract contrasts.
   lmbds = NULL
-  if (!missing(contrasts)) {
+  if (!is.null(contrasts)) {
     if (is.null(names(contrasts)))
       stop("'contrasts' must be a named vector")
     
