2a117d2f76b04d5da7cafc8d5e2e854d42e28581
Daniel Gerlanc <dgerlanc@gmail.com>
2012-04-12 14:53:18 -0400

    Additional checks for the scaleLambdasBySide function.

--- inst/tests/runit.scaleLambdasBySide.R
+++ inst/tests/runit.scaleLambdasBySide.R
@@ -4,11 +4,24 @@
 test.scaleLambdasBySide <- function() {
   ## Test the functioning of the scaleLambdasBySide function
 
-  ## Simplest Case
+  ## Simplest Case and its permutation
   x1   = c(-1, 1)
   res1 = bootES:::scaleLambdasBySide(x1)
   checkEquals(x1, res1)
+  
+  x1a   = c(1, -1)
+  res1a = bootES:::scaleLambdasBySide(x1a)
+  checkEquals(x1a, res1a)
 
+  ## Simplest Case w/ names and its permutation
+  x1na  = c(A=-1, B=1)
+  res1na = bootES:::scaleLambdasBySide(x1na)
+  checkEquals(x1na, res1na)
+  
+  x1nb   = c(B=-1, A=1)
+  res1nb = bootES:::scaleLambdasBySide(x1nb)
+  checkEquals(x1nb, res1nb)
+  
   x2   = c(-0.33, 0.33)
   res2 = bootES:::scaleLambdasBySide(x2)
   checkEquals(res2, c(-1, 1))
